a98cb60946fb20b0bab0fc529320d5cad6f469e1
Kaelah Smallwood <kaelahs@kaelahs-mbp.attlocal.net>
2022-03-07 13:39:01 -0600

    Finished Project

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+    array_length = array.length
+    all_swapped = nil
+    loop do
+        array.each_index do |index|
+            unless index == array.length - 1
+                if array[index] > array[index + 1]
+                    array[index], array[index + 1] = array[index + 1], array[index]
+                end
+            end 
+        end
+        if array.each_cons(2).all? { |a, b| (a <=> b) <= 0}
+            break
+        end
+    end
+    return array
+end
+
+p bubble_sort([4, 3, 1, 1, 17, 83, 23, 0, 5, 100])
